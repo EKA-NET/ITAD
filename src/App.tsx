c9d2f2180075c198f65e18bb874d51a73fb0288c
Michał Puchyr <michalpuchyr3@gmail.com>
2024-04-12 23:28:14 +0200

    Port from Astro to React

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/sponsors/" element={<Sponsors />} />
         </Route>
       </Routes>
     </BrowserRouter>
