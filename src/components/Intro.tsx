e2880f3ccbbcab22c99df175ae5af95ff5276a00
Michał Puchyr <michalpuchyr3@gmail.com>
2024-04-16 22:26:44 +0200

    Add microsoft blocks to main page

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,14 +1,20 @@
 import '../styles/Intro.scss'
 import { CodeWriter } from "./CodeWriter";
+import MicrosoftBlocks from '../assets/microsoft_blocks.svg'
 
 export const Intro = () => {
   return (
-    <section className="intro">
-      <div className="header-container">
-        <h1 className="intro-header">IT Academic Day 2024</h1>
-        <div className="date">11 maja 2024r. 9:00 budynek C-13 Politechnika Wrocławska</div>
-        <CodeWriter />
+    <>
+      <section className="intro">
+        <div className="header-container">
+          <h1 className="intro-header">IT Academic Day 2024</h1>
+          <div className="date">11 maja 2024r. 9:00 budynek C-13 Politechnika Wrocławska</div>
+          <CodeWriter />
+        </div>  
+      </section>
+      <div className="microsoft-blocks">
+        <img src={MicrosoftBlocks} alt="Microsoft Blocks" />
       </div>
-    </section>
+    </>
   );
 };
